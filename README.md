3bd0fc363ea25faf98c3fe7e038a445bca662bf8
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-08-11 22:52:30 +1200

    task1 doen

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# node.js-website
+# Node-JS-Website
+
+### Task 1
+Continue to develop our node js server to be able to handle more requests.
+1. It should be able to handle any js file
+2. It should be able to handle image types (jpg, jpeg, png, gif, etc.) choose at least 2
+3. Include at least 3 more html pages and link them all together in a navigation (it can just be a ul)
+4. Each page should include at least 2 images with a different image type
+
+### Task 2
+(Please make sure you try and complete the Quiz Task first before completing this task as you will be combining them together)  
+Using a front end form with a POST request, combine all our node js tasks to create a front end version of your Quiz.  
+At the end of the form you want an .txt file to output with the same format as your original form.
--- server.js
+++ server.js
@@ -1,41 +1,84 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+var qs = require('querystring');
 
 http.createServer(function(req, res){
   console.log(`${req.method} request for ${req.url}`);
-  if(req.url === '/'){
-    fs.readFile('./public/index.html', 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    })
-  } else if(req.url.match(/.css/)){
-    const cssPath = path.join(__dirname,'public', req.url);
-    fs.readFile(cssPath, 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.end(data);
-    })
-  } else if(req.url.match(/.js/)){
-    const jsPath = path.join(__dirname,'public', req.url);
-    fs.readFile(jsPath, 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/js'});
-      res.end(data);
-    })
-  } else if (req.url.match(/.jpg/)) {
-    const jpgPath = path.join(__dirname,'public', req.url);
-    fs.readFile(jpgPath, function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'jpg'});
-      res.end(data);
-    })
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('404 file not found');
-  }
-
+  if (req.method === 'GET') {
+    if(req.url === '/'){
+      fs.readFile('./public/index.html', 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      })
+    } else if (req.url.match(/.html/)){
+      const pagePath = path.join(__dirname,'public',req.url);
+      fs.readFile(pagePath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      })
+    } else if(req.url.match(/node_modules/)){
+      const modulePath = path.join(__dirname, req.url);
+      fs.readFile(modulePath, 'UTF-8', function(err, data){
+          if (err) throw err;
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          res.end(data);
+      })
+    } else if(req.url.match(/.css/)){
+      const cssPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(cssPath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      })
+    } else if(req.url.match(/.js/)){
+      const jsPath = path.join(__dirname,'public', req.url);
+      fs.readFile(jsPath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/js'});
+        res.end(data);
+      })
+    } else if(req.url.match(/.jpeg/)){
+      const imgPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(imgPath, function(err, data){
+          if(err) throw err;
+          res.writeHead(200, {'Content-Type': 'images/jpeg'});
+          res.end(data);
+      })
+    } else if (req.url.match(/.png/)) {
+      const pngPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(pngPath, function(err,data){
+        if(err) throw err;
+        res.writeHead(200,{'Content-Type': 'images/png'});
+        res.end(data);
+      })
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('404 file not found');
+    }
+  } else if(req.method === 'POST'){
+        // All of the post requests will go here
+        // on our index.html file we have a form with a post route which goes to /sendForm
+        if(req.url === '/sendForm'){
+            // Eventually we will be dealing with large amounts of data
+            // Our servers won't be able to handle all the data at onces
+            // The .on('data') function will trigger as many times as it needs to process all the data
+            // The variable of body will hold all the 'chunks' of data and once it is completed the .on('end') function will trigger
+            let body = '';
+            req.on('data', function(data){
+                body += data;
+            })
+            req.on('end', function(){
+                console.log('at the end');
+                console.log(body.toString());
+                // the querystring module (qs) converts the body variable into a js object which we can them use our normal JS on
+                const formData = qs.parse(body.toString());
+                console.log(formData);
+            })
+        }
+    }
 }).listen(3000);
 
 console.log('The server is running on port 3000');
